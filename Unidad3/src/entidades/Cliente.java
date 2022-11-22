b7da556d13ddae4978d8357c024db25a7d93da31
Marialessandra <maria.mejia14680@est.uca.edu.ni>
2022-11-22 15:58:39 -0600

    A

---
+++ Unidad3/src/entidades/Cliente.java
@@ -0,0 +1,26 @@
+package models;
+
+
+/**
+ * @author Alessandra
+ * @version 1.0
+ * @created 22-nov.-2022 15:30:15
+ */
+public class Cliente {
+
+	private int codigo;
+	private String nombres;
+	private String apellidos;
+	private String direccion;
+	private String telefono;
+	private String email;
+	public Factura m_Factura;
+
+	public Cliente(){
+
+	}
+
+	public void finalize(){
+
+	}
+}//end Cliente
---
+++ Unidad3/src/entidades/Producto.java
@@ -0,0 +1,60 @@
+package models;
+
+
+/**
+ * @author Alessandra
+ * @version 1.0
+ * @created 22-nov.-2022 15:30:15
+ */
+public class Producto {
+
+	private int codigo;
+	private String nombre;
+	private double precio;
+
+	public Producto(){
+
+	}
+
+	public void finalize(){
+
+	}
+
+	public Factura(){
+
+	}
+
+	/**
+	 * 
+	 * @param List<Producto>
+	 */
+	public double calcTotal(List<Producto> List<Producto>){
+		return 0;
+	}
+
+	/**
+	 * 
+	 * @param double
+	 * @param double_dos
+	 */
+	public double aplicarIVA(double double, double double_dos){
+		return 0;
+	}
+
+	/**
+	 * 
+	 * @param double
+	 * @param double_dos
+	 */
+	public double calcMonto(double double, double double_dos){
+		return 0;
+	}
+
+	/**
+	 * 
+	 * @param producto
+	 */
+	public void agregarProducto(Producto producto){
+
+	}
+}//end Producto
